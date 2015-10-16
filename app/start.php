40ec684c1743a2744489291a9fa8cacf548de660
Jan <janliber1@gmail.com>
2015-10-17 00:02:41 +0200

    Fixed double slashes "//" appearing in URL

--- app/start.php
+++ app/start.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 'On');
 
 define('APP_ROOT', __DIR__);
 define('VIEW_ROOT', APP_ROOT . '/views');
-define('BASE_URL', 'http://localhost/cms/');
+define('BASE_URL', 'http://localhost/cms');
 
 try {
     $db = new PDO('mysql:host=127.0.0.1;dbname=cms', 'root', '');
