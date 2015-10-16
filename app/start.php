3e9c31560d5896cca43e186e9cc6bd3b04a5d88c
Jan <janliber1@gmail.com>
2015-10-16 22:09:01 +0200

    Added global footer and header

--- app/start.php
+++ app/start.php
@@ -3,6 +3,13 @@
 ini_set('display_errors', 'On');
 
 define('APP_ROOT', __DIR__);
+define('VIEW_ROOT', APP_ROOT . '/views');
 define('BASE_URL', 'http://localhost/cms/');
 
-$db = new PDO('mysql:host=localhost:dbname=cms', 'root', '');
+try {
+    $db = new PDO('mysql:host=127.0.0.1;dbname=cms', 'root', '');
+    echo "Connected!" ."<br>";
+} catch (Exception $ex) {
+    echo "Connection to database failed!";
+    echo $ex . "<br>";
+}
---
+++ app/views/home.php
@@ -0,0 +1,3 @@
+<?php require VIEW_ROOT . '/templates/header.php'; ?>
+Home
+<?php require VIEW_ROOT . '/templates/footer.php'; ?>
